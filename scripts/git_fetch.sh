5bd11c780ac444abe965f764e0c9eaf9d51afc1a
RyotaMoriyama <info@ryotamoriyama.com>
2019-07-04 20:40:58 +0900

    Modify

--- scripts/git_fetch.sh
+++ scripts/git_fetch.sh
@@ -5,6 +5,7 @@ BRANCH=$2
 
 cd ${REPO_ROOT}
 
+git reset --hard > /dev/null 2>&1
 git checkout ${BRANCH} > /dev/null 2>&1
 git reset --hard > /dev/null 2>&1
 git fetch origin ${BRANCH} > /dev/null 2>&1
