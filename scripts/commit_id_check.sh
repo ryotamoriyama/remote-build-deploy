f67f24c436c97de0390d6adc3b1619a00333403d
RyotaMoriyama <info@ryotamoriyama.com>
2019-07-04 20:02:46 +0900

    Modify

--- scripts/commit_id_check.sh
+++ scripts/commit_id_check.sh
@@ -5,6 +5,9 @@ BRANCH=$2
 
 cd ${REPO_ROOT}
 
+git reset --hard
+git checkout ${BRANCH}
+
 RECENT=$(echo $(git ls-remote origin ${BRANCH}) | sed -e "s/[ \t]\{1,\}.*\$//g")
 LOCAL=$(git rev-parse HEAD)
 
